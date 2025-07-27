df712d5aeb66c46ce706c8b03d620bb4a7f587d9
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-07-27 22:03:03 +0200

    icon in place next working on delete and set priority functionality first need to implement priority in list-item

--- src/Controller.js
+++ src/Controller.js
@@ -76,7 +76,8 @@ class Controller {
         const optionsContainer = document.createElement('div');
         optionsContainer.classList.add('list-item-options-container');
         
-        const icon = document.createElement('img');
+        const icon = document.createElement('div');
+        icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>'
         icon.classList.add('list-item-options-icon');
         optionsContainer.append(icon);
         
